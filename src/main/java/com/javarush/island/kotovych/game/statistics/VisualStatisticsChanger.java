fd1f08312b0dcc794f08ca14ac0f12cd37d98574
RomanKotovych <kotovich.roman2010@gmail.com>
2024-01-17 22:51:58 -0600

    Fixed IllegalStateArgument

--- src/main/java/com/javarush/island/kotovych/game/statistics/VisualStatisticsChanger.java
+++ src/main/java/com/javarush/island/kotovych/game/statistics/VisualStatisticsChanger.java
@@ -2,6 +2,7 @@ package com.javarush.island.kotovych.game.statistics;
 
 import com.javarush.island.kotovych.game.GameScene;
 import com.javarush.island.kotovych.util.EmojiTable;
+import javafx.application.Platform;
 import javafx.scene.control.Label;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -16,7 +17,7 @@ public class VisualStatisticsChanger{
     }
 
     public void update(AtomicInteger totalValue, Map<String, AtomicInteger> totalOrganismCount) {
-        totalOrganisms.setText(String.valueOf(totalValue.get()));
+        Platform.runLater(() -> totalOrganisms.setText(String.valueOf(totalValue.get())));
         StringBuilder builder = new StringBuilder();
         for (Map.Entry<String, AtomicInteger> entry : totalOrganismCount.entrySet()) {
             int value = entry.getValue().get();
@@ -24,6 +25,6 @@ public class VisualStatisticsChanger{
                 builder.append("%s: %d\n".formatted(EmojiTable.getEmoji(entry.getKey()), entry.getValue().get()));
             }
         }
-        organismCount.setText(builder.toString());
+        Platform.runLater(() -> organismCount.setText(builder.toString()));
     }
 }
