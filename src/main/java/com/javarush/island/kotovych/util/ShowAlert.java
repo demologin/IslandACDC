54589f60e7bef68b9ae59c16016812914eb7f969
RomanKotovych <kotovich.roman2010@gmail.com>
2024-01-20 10:06:27 -0600

    Fixed infinite alerts

--- src/main/java/com/javarush/island/kotovych/util/ShowAlert.java
+++ src/main/java/com/javarush/island/kotovych/util/ShowAlert.java
@@ -10,42 +10,51 @@ import java.io.PrintWriter;
 import java.io.StringWriter;
 
 public class ShowAlert {
+    private static boolean blocked = false;
     public static void showErrorAlert(String message) {
-        Alert alert = new Alert(Alert.AlertType.ERROR);
-        alert.setTitle("Error");
-        alert.setHeaderText(null);
-        alert.setContentText(message);
-        alert.showAndWait();
+        if(!blocked) {
+            blocked = true;
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Error");
+            alert.setHeaderText(null);
+            alert.setContentText(message);
+            alert.showAndWait();
+            blocked = false;
+        }
     }
 
     public static void showErrorWithStacktrace(String message, Exception ex) {
-        Alert alert = new Alert(Alert.AlertType.ERROR);
-        alert.setTitle(Constants.EXCEPTION);
-        alert.setHeaderText(null);
-        alert.setContentText(message);
+        if(!blocked) {
+            blocked = true;
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle(Constants.EXCEPTION);
+            alert.setHeaderText(null);
+            alert.setContentText(message);
 
-        StringWriter sw = new StringWriter();
-        PrintWriter pw = new PrintWriter(sw);
-        ex.printStackTrace(pw);
-        String exceptionText = sw.toString();
+            StringWriter sw = new StringWriter();
+            PrintWriter pw = new PrintWriter(sw);
+            ex.printStackTrace(pw);
+            String exceptionText = sw.toString();
 
-        Label label = new Label(Constants.EXCEPTION_STACKTRACE_WAS_TEXT);
+            Label label = new Label(Constants.EXCEPTION_STACKTRACE_WAS_TEXT);
 
-        TextArea textArea = new TextArea(exceptionText);
-        textArea.setEditable(false);
-        textArea.setWrapText(true);
+            TextArea textArea = new TextArea(exceptionText);
+            textArea.setEditable(false);
+            textArea.setWrapText(true);
 
-        textArea.setMaxWidth(Double.MAX_VALUE);
-        textArea.setMaxHeight(Double.MAX_VALUE);
-        GridPane.setVgrow(textArea, Priority.ALWAYS);
-        GridPane.setHgrow(textArea, Priority.ALWAYS);
+            textArea.setMaxWidth(Double.MAX_VALUE);
+            textArea.setMaxHeight(Double.MAX_VALUE);
+            GridPane.setVgrow(textArea, Priority.ALWAYS);
+            GridPane.setHgrow(textArea, Priority.ALWAYS);
 
-        GridPane expContent = new GridPane();
-        expContent.setMaxWidth(Double.MAX_VALUE);
-        expContent.add(label, 0, 0);
-        expContent.add(textArea, 0, 1);
-        alert.getDialogPane().setExpandableContent(expContent);
+            GridPane expContent = new GridPane();
+            expContent.setMaxWidth(Double.MAX_VALUE);
+            expContent.add(label, 0, 0);
+            expContent.add(textArea, 0, 1);
+            alert.getDialogPane().setExpandableContent(expContent);
 
-        alert.showAndWait();
+            alert.showAndWait();
+            blocked = false;
+        }
     }
 }
