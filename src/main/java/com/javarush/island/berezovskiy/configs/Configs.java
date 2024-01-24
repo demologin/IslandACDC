e9c99b2a6882eedc89337cb8dc6602cf451d3acf
Sergei Berezovskii <sergey1berezovskiy@gmail.com>
2024-01-24 22:28:31 +0300

    Project is finished 1.0

--- src/main/java/com/javarush/island/berezovskiy/configs/Configs.java
+++ src/main/java/com/javarush/island/berezovskiy/configs/Configs.java
@@ -12,7 +12,7 @@ public class Configs {
     public static final int ANIMAL_LIMIT_IN_CELL = 4;
     public static final int PREDATORS_COUNT_IN_START = 20;
     public static final int HERBIVORES_COUNT_IN_START = 40;
-    public static final int PLANTS_COUNT_IN_START = 55;
+    public static final int PLANTS_COUNT_IN_START = 30;
     public static final int MIN_ANIMAL_IN_FLOCK = 2;
     public static final Map<String, Integer> ORGANISMS_COUNT_IN_START = Map.of(
             Constants.PREDATOR, PREDATORS_COUNT_IN_START,
